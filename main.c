9be56111960c0d0dc132dcbfeb9f3b1f828ffaa5
NPKhanh <phuongkhanh.nguyen@gmx.de>
2015-08-31 15:04:39 +0200

    neue main.c 2.0
    externe deklarationen von den CAN-Funktionen werden nicht erkannt

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <LPC23xx.h> 
+#include <string.h>
+
 //#include "CAN.h"
 //#include "LPC2xxx_embedded.h"
 #include "emb1_can_labs.h"
@@ -17,8 +19,9 @@ extern void UART_PutChar (unsigned int PortNum, unsigned char Word);
 //Empfangen von Text am Microcontroller
 extern unsigned char UART_GetChar (int PortNum);
 extern void init_VIC(void);
-
 extern void CAN_Set_Acceoptance_Filter_Mode(uint32_t ACF_mode);
+//extern uint32_t CAN_Send_Message(uint32_t CAN_Module_Number, CAN_MSG  *Tx_Data);
+//extern uint32_t CAN_Init( uint32_t CAN_Module_Number, uint32_t CAN_Baud_Rate );
 
 //CAN Einstellungen
 CAN_MSG   message1 = {0x456, {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88}, 8, 0,0};
@@ -35,54 +38,30 @@ int test = 0;
 
 //char* SendingText = "01234567ABCDEFGHJ\n"; 
 
-
-//char* send_whole_text (size_t UART_Num, int length, char* SendText)	//SendText zeigt auf SendingText (gewünschter Text)
-//{
-//	int i = 0;
-//	for (i = 0; i < length; i++)
-//	{
-//		UART_PutChar ( (int) UART_Num, SendText[i]);
-//	}
-//	
-//	return SendText + sizeof(char)*length;
-//	
-//}
-
-
-
-char* send_char(char* val, uint8_t length, uint8_t id, uint8_t CAN_port)
+char* send_char(char* val,uint8_t length,uint8_t id,uint8_t CAN_port)
 {
 	uint32_t test;
 	
-//	CAN_MSG* sending_msg;
-//	
-//	send_msg->data = val; 
-//	send_msg->length = length; 
-//	send_msg->id = id;
-//	send_msg-> frame_format = 0x0; 
-//	send_msg->frame_type = 0x0;
-//	
-//	test = CAN_Send_Message (CAN_port,send_msg);
+	CAN_MSG* send_msg;
+		
+	memcpy(send_msg->data,val,sizeof(char)*length);
+	send_msg->length = length;
+	send_msg->id = id;
 	
-	if (test == True)
+	send_msg->frame_format = 0x0; ///Kann so bleiben
+	send_msg->frame_type = 0x0; ///Kann so bleiben	
+		
+	test = CAN_Send_Message(CAN_port,send_msg);
+
+	if(test == True)
 	{
-		return val+ sizeof(char)*length;
+		return val + sizeof(char) * length;
 	}
 	else
 	{
 		return NULL;
 	}
 }
-
-
-
-void VIC_init (void)
-{
-	VICVectAddr7 = (unsigned long) send_char;
-	VICVectPriority7 = 14;
-	VICIntEnable |= 1 << 7;
-}
-
 
 
 char* read_whole_text( size_t UART_Num, int length, char* ReceivedText)
@@ -98,6 +77,29 @@ char* read_whole_text( size_t UART_Num, int length, char* ReceivedText)
 }
 
 
+
+void read_UART_send_CAN(void) __irq
+{
+	
+	char myText[8];
+	
+	if(read_whole_text(1,8,myText) != NULL)
+	{
+		send_char(myText,8,0x456,1); 
+	}
+	
+	T0IR = 0xff; //reset maybe can be discarded
+	VICVectAddr = 0;
+}
+
+
+
+void VIC_init (void)
+{
+	VICVectAddr7 = (unsigned long) read_UART_send_CAN;
+	VICVectPriority7 = 14;
+	VICIntEnable |= 1 << 7;
+}
 
 
 
@@ -118,9 +120,7 @@ void right_direct(size_t UART_NUM, int amount)
 		{
 			U0RBR = 0x61 + i;
 		}
-	}
-		
-	
+	}	
 }
 
 
@@ -134,17 +134,18 @@ int main (void)
 	unsigned char bef = 0x808080;
 	
 	PortNum = 0;
+
 	UDL = 0x8;
 	UART_mode = 0x03;
-	FIFO_mode = 0x87;
+	FIFO_mode = 0x8B;	//UxFCR für FIFO liegt in 0-7 Bit. Bit 6 und 7 sind für Trigger level Einstellungen (vorher 0x87 = 4 character)
 	IntEnableValue = 0x01;
 	
 	CAN_Init(0,0x3DC002); // Initialize CAN1 module e.g. Data rate value = 0x3DC002 @ 14,4 MHz 
 	CAN_Init(1,0x3DC002); // Initialize CAN2 module e.g. Data rate value = 0x3DC002 @ 14,4 MHz 
 	CAN_Set_Acceptance_Filter_Mode(ACCF_ON);
 	
-	UART_init (0, UDL, UART_mode, (IntEnableValue << 8)| FIFO_mode);
-	UART_init (1, UDL, UART_mode, (IntEnableValue << 8)| FIFO_mode);
+	UART_init (0, UDL, UART_mode, (IntEnableValue << 8 )| FIFO_mode);
+	UART_init (1, UDL, UART_mode, (IntEnableValue << 8 )| FIFO_mode);
 	VIC_init(); 
 
 	
@@ -157,9 +158,9 @@ int main (void)
 //	S_TextPosition = send_whole_text(0,8,S_TextPosition);
 
 
+	//right_direct(1,9);
 	right_direct(1,8);
-
-	R_TextPosition = read_whole_text (1,8,TextReceived);	// Just the position until where in String it got + receive the String send from UART0
+//	R_TextPosition = read_whole_text (1,8,TextReceived);	// Just the position until where in String it got + receive the String send from UART0
 	
 	
 	//read_whole_text(1,8,R_TextPosition);
@@ -170,6 +171,9 @@ int main (void)
 	
 	while (1)
 	{
+		
+		//CAN_Receive_Message(2,&My_CAN2_RX_message);
+
 	}
 	
 }
